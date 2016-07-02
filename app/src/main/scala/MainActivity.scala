5c8eef2452c72f2b695a3ee82533ec6c8c728eea
cielleech <lancelot.q.lee@outlook.com>
2016-07-02 12:28:35 +0800

    add new

---
+++ app/src/main/scala/MainActivity.scala
@@ -0,0 +1,11 @@
+package com.example.cielleech.geoquiz
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity extends AppCompatActivity {
+  override protected def onCreate(savedInstanceState: Bundle): Unit = {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
